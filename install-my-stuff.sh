c0763d196c0783f662ed88f6e58412180da60f28
Mikey Jay <mikeyjay39@gmail.com>
2024-12-28 20:46:05 +0000

    Add stuff

--- install-my-stuff.sh
+++ install-my-stuff.sh
@@ -5,5 +5,21 @@ sudo pacman -S git
 sudo pacman -S man-db
 sudo pacman -S stow
 sudo pacman -S btop
+sudo pacman -Sy neovim
+sudo pacman -S ttf-jetbrains-mono-nerd
+fc-cache -fv
+#sudo pacman -S zsh
+#sudo chsh -s /usr/bin/zsh
+sudo pacman -S k9s
+
+# kaf
+if which kaf >/dev/null 2>&1; then
+	echo kaf already installed
+else
+curl https://raw.githubusercontent.com/birdayz/kaf/master/godownloader.sh | BINDIR=$HOME/bin bash
+sudo mv /home/mikeyjay/bin/kaf /usr/bin/kaf
+rmdir /home/mikeyjay/bin
+fi
+
 
 stow -d ~/dotfiles -t ~ .
