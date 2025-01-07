c72fa7a027c2dfc4d66702efdbb1c390c961d980
Mikey Jay <mikeyjay39@gmail.com>
2025-01-07 19:48:29 +0100

    Cleanup debug config

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -70,7 +70,7 @@ return {
 			-- online, please don't ask me how to install them :)
 			ensure_installed = {
 				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
+				-- "delve",
 				-- "node2",
 				"pwa-node",
 				"neotest,",
@@ -114,15 +114,6 @@ return {
 		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 		--dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 		--dap.listeners.before.event_exited["dapui_config"] = dapui.close
-
-		-- Install golang specific config
-		require("dap-go").setup({
-			delve = {
-				-- On Windows delve must be run attached or it crashes.
-				-- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-				detached = vim.fn.has("win32") == 0,
-			},
-		})
 
 		-- require("dap-vscode-js").setup({
 		-- 	-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
