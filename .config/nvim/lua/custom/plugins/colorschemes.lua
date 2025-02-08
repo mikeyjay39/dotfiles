2c1cf7fbacfa27bc89909c6d7aedf0763a27be21
Mikey Jay <mikeyjay39@gmail.com>
2025-02-07 16:36:48 -0800

    Update

--- .config/nvim/lua/custom/plugins/colorschemes.lua
+++ .config/nvim/lua/custom/plugins/colorschemes.lua
@@ -1,29 +1,29 @@
 return {
-	-- { -- You can easily change to a different colorscheme.
-	-- 	-- Change the name of the colorscheme plugin below, and then
-	-- 	-- change the command in the config to whatever the name of that colorscheme is.
-	-- 	--
-	-- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	-- 	"folke/tokyonight.nvim",
-	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	-- 	init = function()
-	-- 		-- Load the colorscheme here.
-	-- 		-- Like many other themes, this one has different styles, and you could load
-	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-	-- 		vim.cmd.colorscheme("tokyonight-night")
-	-- 		-- vim.cmd.colorscheme("tokyonight-day")
-	--
-	-- 		-- You can configure highlights by doing something like:
-	-- 		vim.cmd.hi("Comment gui=none")
-	-- 	end,
-	-- 	opts = {
-	-- 		transparent = true,
-	-- 		styles = {
-	-- 			sidebars = "transparent",
-	-- 			floats = "transparent",
-	-- 		},
-	-- 	},
-	-- },
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-day")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
 	-- {
 	-- 	"sonph/onehalf",
 	-- 	lazy = false,
@@ -96,6 +96,27 @@ return {
 	-- 	end,
 	-- },
 	-- {
+	-- 	"projekt0n/github-nvim-theme",
+	-- 	name = "github-theme",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		require("github-theme").setup({
+	-- 			options = {
+	-- 				transparent = true,
+	-- 			},
+	-- 			-- palettes = {
+	-- 			-- 	all = {
+	-- 			-- 		Visual = { bg = "#000000", guibg = "#000000", sel1 = "#000000" }, -- Change to your preferred selection color
+	-- 			-- 	},
+	-- 			-- },
+	-- 		})
+	-- 		vim.cmd("colorscheme github_light_colorblind")
+	-- 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "#d9d2e9" }) -- Change color here
+	-- 		vim.api.nvim_set_hl(0, "Visual", { bg = "#7eb6e1" }) -- Change color here
+	-- 	end,
+	-- },
+	-- {
 	-- 	-- ATOM LIGHT
 	-- 	"navarasu/onedark.nvim",
 	-- 	priority = 1000, -- Ensure it loads first
@@ -104,29 +125,12 @@ return {
 	-- 		require("onedark").setup({
 	-- 			style = "light", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 	-- 			transparent = true, -- show/hide background
+	-- 			highlights = {
+	-- 				CursorLine = { bg = "#d9d2e9" },
+	-- 				Visual = { bg = "#7eb6e1" },
+	-- 			},
 	-- 		})
 	-- 		require("onedark").load()
 	-- 	end,
 	-- },
-	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = true,
-				},
-				-- palettes = {
-				-- 	all = {
-				-- 		Visual = { bg = "#000000", guibg = "#000000", sel1 = "#000000" }, -- Change to your preferred selection color
-				-- 	},
-				-- },
-			})
-			vim.cmd("colorscheme github_light_colorblind")
-			vim.api.nvim_set_hl(0, "CursorLine", { bg = "#d9d2e9" }) -- Change color here
-			vim.api.nvim_set_hl(0, "Visual", { bg = "#7eb6e1" }) -- Change color here
-		end,
-	},
 }
