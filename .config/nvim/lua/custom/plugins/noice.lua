a1a7a25adbc14b05a1ebfaab47df72f9a9cdb438
Mikey Jay <mikeyjay39@gmail.com>
2025-02-21 12:27:38 -0800

    Fix noice config
    Show @recording messages as a notify message

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -32,6 +32,13 @@ return {
 					inc_rename = false, -- enables an input dialog for inc-rename.nvim
 					lsp_doc_border = false, -- add a border to hover docs and signature help
 				},
+				-- Show @recording messages as a notify message
+				routes = {
+					{
+						view = "notify",
+						filter = { event = "msg_showmode" },
+					},
+				},
 			})
 		end,
 	},
