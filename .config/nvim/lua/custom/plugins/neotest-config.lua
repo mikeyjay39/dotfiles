40eddd75a4be1fc17d840a4cd8b6df88b183483d
Mikey Jay <mikeyjay39@gmail.com>
2025-03-20 17:28:36 -0700

    Use default neotest-jest config

--- .config/nvim/lua/custom/plugins/neotest-config.lua
+++ .config/nvim/lua/custom/plugins/neotest-config.lua
@@ -6,12 +6,12 @@ M.setup = function()
 	neotest.setup({
 		adapters = {
 			require("neotest-jest")({
-				jestCommand = "./node_modules/jest/bin/jest.js",
-				jestConfigFile = "/home/mikeyjay/omskit/jest.config.js",
-				env = { CI = true },
-				cwd = function(path)
-					return vim.fn.getcwd()
-				end,
+				-- jestCommand = "./node_modules/jest/bin/jest.js",
+				-- jestConfigFile = "/home/mikeyjay/omskit/jest.config.js",
+				-- env = { CI = true },
+				-- cwd = function(path)
+				-- 	return vim.fn.getcwd()
+				-- end,
 			}),
 			require("neotest-java")({
 				{
