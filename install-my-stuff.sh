e76067560bfcfd85ab18f37c44e274d2cc612d0e
Mikey Jay <mikeyjay39@gmail.com>
2025-05-26 21:51:36 -0700

    Add node and nvm

--- install-my-stuff.sh
+++ install-my-stuff.sh
@@ -102,4 +102,25 @@ cd ..
 rm -rf yay
 fi
 
+# nvm
+if which node >/dev/null 2>&1; then
+	echo node already installed
+else
+    # Download and install nvm:
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    
+    # in lieu of restarting the shell
+    \. "$HOME/.nvm/nvm.sh"
+    
+    # Download and install Node.js:
+    nvm install 22
+    
+    # Verify the Node.js version:
+    node -v # Should print "v22.16.0".
+    nvm current # Should print "v22.16.0".
+    
+    # Verify npm version:
+    npm -v # Should print "10.9.2".
+fi
+
 stow -d ~/dotfiles -t ~ .
