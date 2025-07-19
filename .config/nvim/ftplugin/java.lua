e41b28c4a937825f425147d1ef99a9c551991ba0
Mikey Jay <michaeljeszenka@valstro.com>
2025-07-18 17:20:18 -0700

    Fix java symbols

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,8 +1,29 @@
 local config = {
-	cmd = { "~/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
+	cmd = { "/home/mikeyjay/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml", "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 	-- root_dir = vim.fn.getcwd(),
+	on_attach = function(client, bufnr)
+		vim.lsp.set_log_level("debug")
+	end,
+	settings = {
+		java = {
+			symbols = {
+				includesourcemethoddeclaratiokns = true,
+			},
+			project = {
+				-- TODO: Remove this
+				-- referencedLibraries = {
+				-- 	"/home/mikeyjay/TKT-14675-java-logs-tracing/.m2/repository/io/opentelemetry/opentelemetry-api/1.32.0/opentelemetry-api-1.32.0-sources.jar",
+				-- },
+			},
+		},
+	},
 }
+
+-- Ensure the JDK sources are available
+-- local jdk_sources = "/usr/lib/jvm/java-21-openjdk/lib/src.zip" -- Update this path to your JDK's src.zip
+-- config.settings.java.project.referencedLibraries = config.settings.java.project.referencedLibraries or {}
+-- table.insert(config.settings.java.project.referencedLibraries, jdk_sources)
 local function find_jars(pattern)
 	local files = vim.fn.glob(vim.fn.expand(pattern), true, true)
 	if type(files) == "string" and files ~= "" then
@@ -26,7 +47,14 @@ local bundles = {}
 vim.list_extend(bundles, java_debug_plugin)
 vim.list_extend(bundles, test_bundles)
 
+-- Add jars from .m2 repository
+-- local m2_jars = vim.fn.globpath("~/TKT-14675-java-logs-tracing/.m2/repository", "**/*.jar", true, true)
+-- vim.notify("Found " .. #m2_jars .. " jars in .m2 repository", vim.log.levels.INFO, { title = "Java LSP" })
+-- -- Extend bundles with .m2 jars
+-- vim.list_extend(bundles, m2_jars)
+
 config.init_options = {
+	logLevel = "INFO", -- Set the desired log level
 	bundles = bundles,
 }
 -- print("Final Root Dir:", config.root_dir)
