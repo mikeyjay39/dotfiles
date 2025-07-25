7a6f6d13c471736378ff5f189eb9b394547aa227
Mikey Jay <mikeyjay39@gmail.com>
2025-07-24 19:46:55 -0700

    Add leetcode.nvim

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -8,6 +8,7 @@ return {
 	require("custom.plugins.copilot"),
 	require("custom.plugins.diffview"),
 	require("custom.plugins.fzf-lua"),
+	require("custom.plugins.leetcode"),
 	require("custom.plugins.neotest"),
 	require("custom.plugins.noice"),
 	require("custom.plugins.nvim-coverage"),
