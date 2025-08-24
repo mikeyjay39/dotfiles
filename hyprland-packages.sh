e9e80fc6a41f7c251a7235bf2cff0a5360b1c0ee
Mikey Jay <mikeyjay39@gmail.com>
2025-08-24 21:13:54 +0200

    Add hypr dir

---
+++ hyprland-packages.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/sh
+
+install_if_not_found() {
+local COMMAND="$1"
+
+if [ -z "$2" ]; then
+	local PACKAGE="$COMMAND"
+else
+	local PACKAGE="$2"
+fi
+
+if which "$COMMAND" >/dev/null 2>&1; then
+	echo "$COMMAND already installed"
+else
+	echo "Installing $PACKAGE"
+sudo pacman -S "$PACKAGE"
+fi
+}
+
+# arg 1 is name of command, arg 2 is package name
+install_with_aur_if_not_found() {
+local COMMAND="$1"
+
+if [ -z "$2" ]; then
+	local PACKAGE="$COMMAND"
+else
+	local PACKAGE="$2"
+fi
+
+if which "$COMMAND" >/dev/null 2>&1; then
+	echo "$COMMAND already installed"
+else
+	echo "Installing $PACKAGE"
+ yay -S "$PACKAGE"
+fi
+
+}
+
+
+install_if_not_found wireplumber
+install_if_not_found nmcli networkmanager
+install_if_not_found nm-applet network-manager-applet
+sudo systemctl enable NetworkManager.service --now
+
+
