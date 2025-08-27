e2c1d8d82c604fedf6794a7f3e2b29d4c955224c
Mikey Jay <mikeyjay39@gmail.com>
2025-08-27 10:04:16 +0200

    Update powermenu with btop

--- .config/wofi/powermenu.sh
+++ .config/wofi/powermenu.sh
@@ -4,8 +4,9 @@ options=" Lock | hyprlock
  Sleep | systemctl suspend
 󰈆 Logout | hyprctl dispatch exit
  Reboot | systemctl reboot
- Shutdown | systemctl poweroff"
+ Shutdown | systemctl poweroff
+ Btop (System Processes) | ghostty -e btop"
 
-choice=$(echo -e "$options" | wofi --show dmenu -p "Power:" -L 5 | cut -d'|' -f2)
+choice=$(echo -e "$options" | wofi --show dmenu -p "Power:" -L 6 | cut -d'|' -f2)
 
 [ -n "$choice" ] && bash -c "$choice"
