e37872d1c087a93460775e1eef9b78e9af954427
Mikey Jay <mikeyjay39@gmail.com>
2026-01-07 11:07:14 -0800

    Fix quickfix for fzf-lua

--- .config/nvim/lua/custom/plugins/fzf-lua.lua
+++ .config/nvim/lua/custom/plugins/fzf-lua.lua
@@ -5,6 +5,16 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		-- or if using mini.icons/mini.nvim
 		-- dependencies = { "echasnovski/mini.icons" },
-		opts = {},
+		opts = {
+			keymap = {
+				fzf = {
+					-- This line maps Ctrl+Q to select all results and then accept them,
+					-- which automatically sends them to the quickfix list.
+					["ctrl-q"] = "select-all+accept",
+					-- You can also ensure the default Ctrl+A is available if needed:
+					["ctrl-a"] = "toggle-all",
+				},
+			},
+		},
 	},
 }
