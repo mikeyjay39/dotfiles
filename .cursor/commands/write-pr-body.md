e1867e9adb817473fca6fd880b73a160d87f921c
Mikey Jay <mikeyjay39@gmail.com>
2026-02-20 13:35:06 -0800

    Add cursor commands

---
+++ .cursor/commands/write-pr-body.md
@@ -0,0 +1,7 @@
+---
+description: Fill out the PR body description using Valstro template.
+---
+
+# Update PR Body Description
+
+Update the PR body description for this branch using the Valstro template.
--- install/setup-symlinks.sh
+++ install/setup-symlinks.sh
@@ -3,3 +3,11 @@ set -Eeuo pipefail
 
 stow -d ${HOME}/dotfiles -t ~ . --adopt
 
+# setup cursor commands
+mkdir -p ${HOME}/.cursor/commands
+for command in ${HOME}/dotfiles/.cursor/commands/*; do
+  target_link="${HOME}/.cursor/$(basename "$command")"
+  if [ ! -L "$target_link" ]; then
+    ln -s "$command" "$target_link"
+  fi
+done
