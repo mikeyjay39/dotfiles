269ca121ae27c0e15efb75deff99412cf5492aff
Mikey Jay <mikeyjay39@gmail.com>
2026-04-20 17:26:29 -0700

    Clean up nvim plugins

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -4,17 +4,26 @@
 -- See the kickstart.nvim README for more information
 return {
 	require("custom.plugins.avante"), -- comment this out and run :Copilot setup to setup copilot
+	require("custom.plugins.nvim-cmp"),
 	require("custom.plugins.colorschemes"),
+	require("custom.plugins.conform"), -- Autoformat
 	require("custom.plugins.copilot"),
 	require("custom.plugins.diffview"),
 	require("custom.plugins.fzf-lua"),
+	require("custom.plugins.gitsigns"),
 	require("custom.plugins.leetcode"),
+	require("custom.plugins.mini"),
 	require("custom.plugins.neotest"),
 	require("custom.plugins.noice"),
 	require("custom.plugins.nvim-coverage"),
+	require("custom.plugins.nvim-dap-virtual-text"),
+	require("custom.plugins.nvim-jdtls"),
+	require("custom.plugins.nvim-treesitter"),
+	require("custom.plugins.plenary"),
 	require("custom.plugins.rustaceanvim"),
 	require("custom.plugins.smear-cursor"),
 	require("custom.plugins.smooth-cursor"),
 	require("custom.plugins.todo-comments"),
+	require("custom.plugins.typescript-tools"),
 	require("custom.plugins.which-key"),
 }
