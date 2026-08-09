c2e6a0e451e06da7d3a41b19fa786ecab6306434
Mikey Jay <mikeyjay39@gmail.com>
2026-08-09 19:48:28 +0200

    Add rclone protondrive alias

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -4,3 +4,7 @@ alias cd='z'
 
 # history for zsh
 alias history='history 0'
+
+# rclone sync with protondrive
+# can add --dry-run flag
+alias proton-drive-docs-backup='rclone copy protondrive:file_cabinet/ /mnt/usb-drive/my_documents/file_cabinet/ --progress'
