edbb0fc7d4363a1edf96795628fe065a71e45952
Mikey Jay <mikeyjay39@gmail.com>
2026-08-13 11:05:56 +0200

    add rm alias for preserve root

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -8,3 +8,6 @@ alias history='history 0'
 # rclone sync with protondrive
 # can add --dry-run flag
 alias proton-drive-docs-backup='rclone copy protondrive:file_cabinet/ /mnt/usb-drive/my_documents/file_cabinet/ --progress'
+
+# rm
+aliast rm='rm --preserve-root'
